90dacc6fd1ca7efe7c46e1fa1324626d47e8f343
Arnav Goel <24ucs029@lnmiit.ac.in>
2026-04-04 21:06:30 +0530

    Clean unnecessary submissions

--- Data Structures & Algorithms/duplicate-integer/submission-2.py
+++
@@ -1,8 +0,0 @@
-class Solution:
-        def hasDuplicate(self, nums: List[int]) -> bool:
-            from collections import Counter
-            count_map = Counter(nums)
-            for key in count_map:
-                if count_map[key] > 1:
-                    return True
-            return False
--- Data Structures & Algorithms/is-anagram/submission-0.py
+++ Data Structures & Algorithms/is-anagram/submission-0.py
@@ -1,9 +1,6 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
 
-        # one line answer
-        # return Counter(s) == Counter(t)
-
         if len(s) != len(t):
             return False
         
